081026d138e02e58b3e108f96dbce8acd0acff4f
Michael Conrad <michael@newsrx.com>
2020-11-03 12:08:36 -0500

    Initial publication

---
+++ src/main/java/org/xBaseJ/annotations/DBFField.java
@@ -0,0 +1,17 @@
+package org.xBaseJ.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface DBFField {
+	String name();
+	int size() default 0;
+	int dec() default 0;
+	boolean optional() default false;
+	boolean rtrim() default true;
+	boolean ltrim() default false;
+}
